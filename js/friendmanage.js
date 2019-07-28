115f0973d0d92e073578ddf9aa2a43cd235089f9
MissWangy <you@example.com>
2019-07-28 15:30:28 +0800

    页面交互功能基本实现

--- js/friendmanage.js
+++ js/friendmanage.js
@@ -1,33 +1,298 @@
-$(function(){
-    var token = localStorage.getItem("token")
-    console.log(token)
-    var data =
+$(function() {
+  layui.use(['form', 'upload', 'laydate'], function() {
+    var form = layui.form,
+      layer = layui.layer,
+      layedit = layui.layedit,
+      laydate = layui.laydate,
+      upload = layui.upload
+
+    //全部
+    form.on('select(category1)', function(data) {
+      category = data.value
+      categoryName = data.elem[data.elem.selectedIndex].text
+      console.log(categoryName)
+      form.render('select')
+    })
+
+    //标签
+    form.on('select(category2)', function(data) {
+      category = data.value
+      categoryName = data.elem[data.elem.selectedIndex].text
+      console.log(categoryName)
+      form.render('select')
+    })
+
+    //搜索好友昵称
+    $('body').delegate('.searchnick', 'propertychange input', function() {
+      console.log($(this).val())
+    })
+
+    token = localStorage.getItem('token')
+    wxid_raw = JSON.parse(localStorage.getItem('wxid_raw'))
+    console.log(token, wxid_raw)
+    for (var id in wxid_raw) {
+      var data =
         '{"type":' +
         '"get_friend_list",' +
         '"wxid":' +
-        '"wxid_seiyx5yxsx7822",' +
+        '"' +
+        wxid_raw[id] +
+        '",' +
         '"limit1":' +
-        '"'+0+'",' +
+        '"' +
+        0 +
+        '",' +
         '"limit2":' +
-        '"'+20+'",' +
+        '"' +
+        20 +
+        '",' +
         '"ticket":' +
-        '"'+token+'"' +
+        '"' +
+        token +
+        '"' +
         '}'
-        //console.log(data)
-        //获取好友列表
-    $.ajax({
+
+      //传入参数
+      console.log(data)
+
+      $.ajax({
         type: 'post',
-        url: '/api.php',
-        data:data,
+        url: 'http://192.168.10.177/api.esp',
+        data: data,
         dataType: 'json',
-        success:function(msg){
-            console.log(msg)
-            if(msg.err_code == 0){
-                console.log(msg.data)
+        success: function(msg) {
+          console.log(msg)
+          if (msg.err_code == 0) {
+            var data = msg.data
+            console.log(data)
+            var str = ''
+            var ind = 0
+            let msglist = []
+            var wxfrom = []
+            for (var id in data) {
+              //存储本人账号对应以及对应的好友账号
+              wxfrom.push({
+                selfwx: data[id].from_wxid,
+                fronwx: data[id].friend_wxid
+              })
+              wxfrom = norepeat(wxfrom)
+              console.log(wxfrom)
+              localStorage.setItem('wxfrom', JSON.stringify(wxfrom))
+
+              msglist.push(data[id].friend_wxid)
+              msglist = norepeat(msglist)
+              str += `
+                            <tr>
+                                    <td>
+                                        <input type="checkbox" name="" lay-skin="primary" lay-filter="itemChoose">
+                                    </td>
+                                    <td>${data[id].friend_wxid}</td>
+                                    <td>
+                                            <img src="${
+                                              data[id].headurl
+                                            }" data-id=${
+                data[id].id
+              } style="width:31px;height:31px;border-radius:50%">
+                                        ${data[id].nickname}</td>
+                                    <td>${data[id].friend_wxid}</td>
+                                    <td>
+                                        <em class="c_blue">同学家庭</em>
+                                    </td>
+                                    <td>${data[id].from_wxid}</td>
+                                    <td>
+                                        <a href="javascript:;" class="lk c_blue chartdetBtn">查看详情</a>
+                                    </td>
+                                    <td>
+                                        <a href="javascript:;" class="lk_ico chart msgBtn" data-friend=${
+                                          data[id].friend_wxid
+                                        } data-nickname=${
+                data[id].nickname
+              }>去聊天</a>
+                                        <a href="javascript:;" class="lk_ico quan chartquanBtn">朋友圈</a>
+                                    </td>
+                                </tr>
+                            `
             }
+            $('tbody').append(str)
+            localStorage.setItem('friend_wxid', JSON.stringify(msglist))
+            form.render('checkbox')
+          }
         },
-        error:function(err){
-            console.log(err)
+        error: function(err) {
+          console.log(err)
         }
+      })
+    }
+
+    form.on('checkbox(allChoose)', function(data) {
+      var child = $(data.elem)
+        .parents('table')
+        .find('tbody input[type="checkbox"]')
+      child.each(function(index, item) {
+        item.checked = data.elem.checked
+      })
+      form.render('checkbox')
     })
+    form.on('checkbox(itemChoose)', function(data) {
+      var sib = $(data.elem)
+        .parents('table')
+        .find('tbody input[type="checkbox"]:checked').length
+      var total = $(data.elem)
+        .parents('table')
+        .find('tbody input[type="checkbox"]').length
+      if (sib == total) {
+        $(data.elem)
+          .parents('table')
+          .find('thead input[type="checkbox"]')
+          .prop('checked', true)
+        form.render()
+      } else {
+        $(data.elem)
+          .parents('table')
+          .find('thead input[type="checkbox"]')
+          .prop('checked', false)
+        form.render()
+      }
+    })
+
+    $('.addtagBtn').click(function() {
+      $('.pop_hy_addtag').show()
+      $('.pop_bg').show()
+    })
+
+    //去聊天
+    $('body').delegate('.msgBtn', 'click', function() {
+      $('.pop_chart_msg').show()
+      $('.pop_bg').show()
+      var $friend = $(this)
+        .get(0)
+        .getAttribute('data-friend')
+      var nickname = $(this)
+        .get(0)
+        .getAttribute('data-nickname')
+      nickname = '好友昵称：' + nickname
+      $('.pop_chart_msg .fl').html(nickname)
+
+      //表情包
+      var count = 0
+      $('.bq').click(function() {
+        count++
+        if (count % 2 == 0) {
+          $('.positiemoij').hide()
+        } else {
+          $('.positiemoij').show()
+        }
+        var content = $('.textarea').val()
+
+        //输入表情
+        $('.positiemoij>.expression li').click(function() {
+          console.log(content)
+          var name = $(this)
+            .find('i')
+            .get(0)
+            .getAttribute('data-name')
+          $('.positiemoij').hide()
+          //$content += $(".ipt").val("["+name+"]")
+          content += '[' + name + ']'
+
+          console.log(content)
+          $('.textarea').val(content)
+        })
+      })
+
+      $('.queding2').click(function() {
+        $('.positiemoij').hide()
+        var $content = $('textarea').val()
+        var data =
+          '{"type":' +
+          '"send_msg",' +
+          '"wxid":' +
+          '"' +
+          wxid_raw +
+          '",' +
+          '"to_wxid":' +
+          '"z1161091470",' +
+          '"content":' +
+          '"' +
+          $content +
+          '",' +
+          '"ticket":' +
+          '"' +
+          token +
+          '"' +
+          '}'
+        //console.log(data)
+
+        $.ajax({
+          type: 'post',
+          url: 'http://192.168.10.177/api.esp',
+          data: data,
+          dataType: 'json',
+          success: function(msg) {
+            //console.log(msg)
+            if (msg.err_code == 0) {
+              $('.pop_hy_addtag1').hide()
+              $('.pop_bg').hide()
+              layer.msg('发送成功')
+            } else {
+              layer.msg('发送失败')
+            }
+            $('textarea').val('')
+          },
+          error: function(err) {
+            console.log(err)
+          }
+        })
+      })
+    })
+    //去朋友圈
+    $('body').delegate('.chartquanBtn', 'click', function() {
+      $('.pop_chart_quan').show()
+      $('.pop_bg').show()
+    })
+
+    $('body').delegate('.chartdetBtn', 'click', function() {
+      $('.pop_chart_det').show()
+      $('.pop_bg').show()
+    })
+
+    // $('.chartdetBtn').click(function(){
+    //     $('.pop_chart_det').show();
+    //     $('.pop_bg').show();
+    // });
+
+    $('.edittagBtn').click(function() {
+      $('.pop_hy_edittag').show()
+      $('.pop_bg').show()
+    })
+  })
+
+  $('.positiemoij>ul li').click(function() {
+    var $ind = $(this).text() - 1
+    console.log($ind)
+    $('.expression')
+      .eq($ind)
+      .addClass('one')
+      .siblings()
+      .removeClass('one')
+  })
+
+  //控制输入的文字数量
+  $('body').delegate('.ipt', 'propertychange input', function() {
+    if ($(this).val().length > 0) {
+      var $len = $(this).val().length
+      var $inputlen = 3000 - $len
+      $('.frs').html('还可输入' + $inputlen + '字，按Enter键换行')
+      if ($len >= 3000) {
+        $(this).val(
+          $(this)
+            .val()
+            .substring(0, 3000)
+        )
+        $('.words').html(0)
+      }
+    } else {
+      console.log('未输入')
+    }
+  })
 })
--- js/server.js
+++ js/server.js
@@ -1,247 +1,267 @@
-$(function(){
-    token = localStorage.getItem("token")
-    wxid_raw = JSON.parse(localStorage.getItem("wxid_raw"))
-    friend_wxid =JSON.parse(localStorage.getItem("friend_wxid"))
-    wxfrom = JSON.parse(localStorage.getItem("wxfrom"))
-   
-    msglist = []
-    let leftmsg = ""
-    for(var i in wxfrom){
-        var data =
-        '{"type":' +
-        '"get_msg",' +
-        '"from_wxid":' +
-        '"'+wxfrom[i].selfwx+'",' +
-        '"to_wxid":' +
-        '"'+wxfrom[i].fronwx+'",' +
-        '"createtime":' +
-        '"0",' +
-        '"ticket":' +
-        '"'+token+'"' +
-        '}'
-        //console.log(data)
-        //获取聊天信息
-        $.ajax({
-            type: 'post',
-            url: 'http://192.168.10.177/api.esp',
-            data:data,
-            dataType: 'json',
-            success:function(msg){
-                if(msg.err_code == 0){
-                    var data = msg.data
-                    console.log(data)                 
-                    msgnick = []
-                    for(m in wxid_raw){
-                        if(data[0]){
-                            if(data[0].from_wxid !== wxid_raw[m]){
-                                leftmsg = `
-                                <li class="item clearfix clickhandler" data-fd=${data[0].to_wxid} data-td=${data[0].from_wxid}>
+$(function() {
+  token = localStorage.getItem('token')
+  wxid_raw = JSON.parse(localStorage.getItem('wxid_raw'))
+  friend_wxid = JSON.parse(localStorage.getItem('friend_wxid'))
+  wxfrom = JSON.parse(localStorage.getItem('wxfrom'))
+
+  msglist = []
+  let leftmsg = ''
+  for (var i in wxfrom) {
+    var data =
+      '{"type":' +
+      '"get_msg",' +
+      '"from_wxid":' +
+      '"' +
+      wxfrom[i].selfwx +
+      '",' +
+      '"to_wxid":' +
+      '"' +
+      wxfrom[i].fronwx +
+      '",' +
+      '"createtime":' +
+      '"0",' +
+      '"ticket":' +
+      '"' +
+      token +
+      '"' +
+      '}'
+    //console.log(data)
+    //获取聊天信息
+    $.ajax({
+      type: 'post',
+      url: 'http://192.168.10.177/api.esp',
+      data: data,
+      dataType: 'json',
+      success: function(msg) {
+        if (msg.err_code == 0) {
+          var data = msg.data
+          console.log(data)
+          msgnick = []
+          for (m in wxid_raw) {
+            if (data[0]) {
+              if (data[0].from_wxid !== wxid_raw[m]) {
+                leftmsg = `
+                                <li class="item clearfix clickhandler" data-fd=${
+                                  data[0].to_wxid
+                                } data-td=${data[0].from_wxid}>
                                     <img src="images/kf_06.png" alt="" class="hdimg fl" />
                                     <div class="info">
-                                    <h6 class="name">${data[0].from_wxid}<em class="num">1</em></h6>
+                                    <h6 class="name">${
+                                      data[0].from_wxid
+                                    }<em class="num">1</em></h6>
                                     <p class="fz ellipsis">留言内容聊天内容留言内...</p>
                                     </div>
                                 </li>
                                 `
-                            }else{
-                                leftmsg = `
-                                <li class="item clearfix clickhandler" data-fd=${data[0].from_wxid} data-td=${data[0].to_wxid}>
+              } else {
+                leftmsg = `
+                                <li class="item clearfix clickhandler" data-fd=${
+                                  data[0].from_wxid
+                                } data-td=${data[0].to_wxid}>
                                     <img src="images/kf_06.png" alt="" class="hdimg fl" />
                                     <div class="info">
-                                    <h6 class="name">${data[0].to_wxid}<em class="num">1</em></h6>
+                                    <h6 class="name">${
+                                      data[0].to_wxid
+                                    }<em class="num">1</em></h6>
                                     <p class="fz ellipsis">留言内容聊天内容留言内...</p>
                                     </div>
                                 </li>
                                 `
-                            }
-                        }
-                    }                  
-                    $(".chart_memlist").append(leftmsg)
-                    // for(i in data){
-                    //     //获取消息来源好友微信号
-                    //     for(m in wxid_raw){
-                    //         //如果data[i].from_wxid不等于本微信号，则添加from
-                    //         if(data[i].from_wxid !== wxid_raw[m]){
-                    //             //console.log(data[i].to_wxid)
-                    //             msglist.push(data[i].from_wxid)
-                    //         }else{
-                    //             //否则添加to微信号 do 1470
-                    //             //console.log(data[i].to_wxid)
-                    //             msglist.push(data[i].to_wxid)
-                    //         }
-                    //     }
-                    // }
-                    // msglist = norepeat(msglist)                
-                    // //左边的好友
-                    // for(let j = 0; j < msglist.length; j++){
-                    //         leftmsg = `
-                    //         <li class="item clearfix clickhandler">
-                    //             <img src="images/kf_06.png" alt="" class="hdimg fl" />
-                    //             <div class="info">
-                    //             <h6 class="name">${msglist[j]}<em class="num">1</em></h6>
-                    //             <p class="fz ellipsis">留言内容聊天内容留言内...</p>
-                    //             </div>
-                    //         </li>
-                    //         `
-                    //     }
-                    //     $(".chart_memlist").append(leftmsg)
-                        
-                }
-                
-            },
-            error:function(err){
-                console.log(err)
+              }
             }
-            
-        })
-    
-        
-    }
+          }
+          $('.chart_memlist').append(leftmsg)
+          // for(i in data){
+          //     //获取消息来源好友微信号
+          //     for(m in wxid_raw){
+          //         //如果data[i].from_wxid不等于本微信号，则添加from
+          //         if(data[i].from_wxid !== wxid_raw[m]){
+          //             //console.log(data[i].to_wxid)
+          //             msglist.push(data[i].from_wxid)
+          //         }else{
+          //             //否则添加to微信号 do 1470
+          //             //console.log(data[i].to_wxid)
+          //             msglist.push(data[i].to_wxid)
+          //         }
+          //     }
+          // }
+          // msglist = norepeat(msglist)
+          // //左边的好友
+          // for(let j = 0; j < msglist.length; j++){
+          //         leftmsg = `
+          //         <li class="item clearfix clickhandler">
+          //             <img src="images/kf_06.png" alt="" class="hdimg fl" />
+          //             <div class="info">
+          //             <h6 class="name">${msglist[j]}<em class="num">1</em></h6>
+          //             <p class="fz ellipsis">留言内容聊天内容留言内...</p>
+          //             </div>
+          //         </li>
+          //         `
+          //     }
+          //     $(".chart_memlist").append(leftmsg)
+        }
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
+  }
 
-
-    $("body").delegate(".clickhandler","click",function(){
-        var fromwxid = $(this).get(0).getAttribute("data-fd")
-        var towxid = $(this).get(0).getAttribute("data-td")
-        //console.log(fromwxid,towxid)
-        var data =
-        '{"type":' +
-        '"get_msg",' +
-        '"from_wxid":' +
-        '"'+fromwxid+'",' +
-        '"to_wxid":' +
-        '"'+towxid+'",' +
-        '"createtime":' +
-        '"0",' +
-        '"ticket":' +
-        '"'+token+'"' +
-        '}'
-        //console.log(data)
-        $(".chart_dialog .list").text("")
-        $.ajax({
-            type: 'post',
-            url: 'http://192.168.10.177/api.esp',
-            data:data,
-            dataType: 'json',
-            success:function(msg){
-                if(msg.err_code == 0){
-                    var data = msg.data
-                    console.log(data)
-                    var str = ""
-                    for(var j in data){
-                        //右边的聊天
-                        //左边
-                        if(data[j].to_wxid == towxid){
-                            str = `
+  $('body').delegate('.clickhandler', 'click', function() {
+    var fromwxid = $(this)
+      .get(0)
+      .getAttribute('data-fd')
+    var towxid = $(this)
+      .get(0)
+      .getAttribute('data-td')
+    //console.log(fromwxid,towxid)
+    var data =
+      '{"type":' +
+      '"get_msg",' +
+      '"from_wxid":' +
+      '"' +
+      fromwxid +
+      '",' +
+      '"to_wxid":' +
+      '"' +
+      towxid +
+      '",' +
+      '"createtime":' +
+      '"0",' +
+      '"ticket":' +
+      '"' +
+      token +
+      '"' +
+      '}'
+    //console.log(data)
+    $('.chart_dialog .list').text('')
+    $.ajax({
+      type: 'post',
+      url: 'http://192.168.10.177/api.esp',
+      data: data,
+      dataType: 'json',
+      success: function(msg) {
+        if (msg.err_code == 0) {
+          var data = msg.data
+          console.log(data)
+          var str = ''
+          for (var j in data) {
+            //右边的聊天
+            //左边
+            if (data[j].to_wxid == towxid) {
+              str = `
                             <li class="odd lists">
                             <img src="images/kf_10.png" alt="" class="hdimg" />
                             <div class="txt">${data[j].content}</div>
                             </li>
                             `
-                        }else{
-                            //右边
-                            str = `
+            } else {
+              //右边
+              str = `
                                 <li class="even lists">
                                 <div class="txt">${data[j].content}</div>
                                 <img src="images/kf_11.png" alt="" class="hdimg" />
                                 </li>
-                                `     
-                                }   
-                        $(".chart_dialog .list").append(str)  
-                        setTimeout(function(){
-                            $(".lists:last-child")[0].scrollIntoView()
-                        },100)
-                    }
-                }
+                                `
             }
-        })
-
-        //点击发送
-       $(".button").unbind("click").bind("click",function(){
-        var data =
-        '{"type":' +
-        '"send_msg",' +
-        '"wxid":' +
-        '"'+wxid_raw+'",' +
-        '"to_wxid":' +
-        '"'+fromwxid+'",' +
-        '"content":' +
-        '"1212",' +
-        '"ticket":' +
-        '"'+token+'"' +
-        '}'
-        console.log(data)
-        $.ajax({
-            type: 'post',
-            url: 'http://192.168.10.177/api.esp',
-            data:data,
-            dataType: 'json',
-            success:function(msg){
-                console.log(msg)
-            }
-        })
-
-       })
-    })
-    
-
-
-        //定时获取发送
-        setInterval(function(){
-            
-            callFunction()
-        },2000)
-
-        var data = '{"type":' +
-        '"get_new_msg",' +
-        '"ticket":' +
-        '"'+token+'"' +
-        '}'
-
-        function callFunction(){
-            // $.ajax({
-            //     type: 'post',
-            //     url: 'http://192.168.10.177/api.esp',
-            //     data:data,
-            //     dataType: 'json',
-            //     success:function(msg){
-            //         console.log(msg)
-            //         if(msg.err_code == 0){
-            //             var content = msg.content
-            //             var from_wxid = msg.from_wxid
-            //             var indexofmsg = msglist.indexOf(from_wxid)
-            //             console.log(msglist.indexOf(from_wxid)) 
-            //             if(indexofmsg == -1){
-            //                 $(".chart_memlist").append(`
-            //                 <li class="item clearfix">
-            //                     <img src="images/kf_06.png" alt="" class="hdimg fl" />
-            //                     <div class="info">
-            //                     <h6 class="name">${from_wxid}<em class="num" data-msgid=${msg.msgid}>1</em></h6>
-            //                     <p class="fz ellipsis">${content}</p>
-            //                     </div>
-            //                 </li>
-            //                 `)
-            //             }else{
-            //                 var fromwxid = msg.from_wxid
-            //                 var havemsg = $("h6:contains(fromwxid)").parents("li")
-            //                 console.log(havemsg)
-                           
-                            
-            //             }
-            //         }
-            //     }
-            // })
+            $('.chart_dialog .list').append(str)
+            setTimeout(function() {
+              $('.lists:last-child')[0].scrollIntoView()
+            }, 100)
           }
+        }
+      }
+    })
+
+    //点击发送
+    $('.button')
+      .unbind('click')
+      .bind('click', function() {
+        $('.positiemoij').hide()
+        var $content = $('textarea').val()
+        var data =
+          '{"type":' +
+          '"send_msg",' +
+          '"wxid":' +
+          '"' +
+          wxid_raw +
+          '",' +
+          '"to_wxid":' +
+          '"' +
+          fromwxid +
+          '",' +
+          '"content":' +
+          '"' +
+          $content +
+          '",' +
+          '"ticket":' +
+          '"' +
+          token +
+          '"' +
+          '}'
+        console.log(data)
+        // $.ajax({
+        //     type: 'post',
+        //     url: 'http://192.168.10.177/api.esp',
+        //     data:data,
+        //     dataType: 'json',
+        //     success:function(msg){
+        //         console.log(msg)
+        //     }
+        // })
+      })
+  })
+
+  //定时获取发送
+  setInterval(function() {
+    callFunction()
+  }, 2000)
+
+  var data =
+    '{"type":' + '"get_new_msg",' + '"ticket":' + '"' + token + '"' + '}'
+
+  function callFunction() {
+    // $.ajax({
+    //     type: 'post',
+    //     url: 'http://192.168.10.177/api.esp',
+    //     data:data,
+    //     dataType: 'json',
+    //     success:function(msg){
+    //         console.log(msg)
+    //         if(msg.err_code == 0){
+    //             var content = msg.content
+    //             var from_wxid = msg.from_wxid
+    //             var indexofmsg = msglist.indexOf(from_wxid)
+    //             console.log(msglist.indexOf(from_wxid))
+    //             if(indexofmsg == -1){
+    //                 $(".chart_memlist").append(`
+    //                 <li class="item clearfix">
+    //                     <img src="images/kf_06.png" alt="" class="hdimg fl" />
+    //                     <div class="info">
+    //                     <h6 class="name">${from_wxid}<em class="num" data-msgid=${msg.msgid}>1</em></h6>
+    //                     <p class="fz ellipsis">${content}</p>
+    //                     </div>
+    //                 </li>
+    //                 `)
+    //             }else{
+    //                 var fromwxid = msg.from_wxid
+    //                 var havemsg = $("h6:contains(fromwxid)").parents("li")
+    //                 console.log(havemsg)
+    //             }
+    //         }
+    //     }
+    // })
+  }
 })
 
-
-function norepeat(arr){
-    for(var i = 0; i < arr.length-1; i++){
-        for(var j = i+1; j < arr.length; j++){
-            if(arr[i]==arr[j]){
-                arr.splice(j,1);
-                j--;
-            }
-        }
+function norepeat(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        arr.splice(j, 1)
+        j--
+      }
     }
-    return arr;
+  }
+  return arr
 }
